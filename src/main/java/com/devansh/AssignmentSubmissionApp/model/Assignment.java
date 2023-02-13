5431b12fd8e809bf1143ab15c07ebb3a308f3590
Devansh Akruvala <devanshakruvala@gmail.com>
2023-02-13 18:03:32 +0530

    Project Init and User and Assignment model created

---
+++ src/main/java/com/devansh/AssignmentSubmissionApp/model/Assignment.java
@@ -0,0 +1,63 @@
+package com.devansh.AssignmentSubmissionApp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Assignment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String status;
+	private String githubUrl;
+	private String branch;
+	private String codeReviewVideoUrl;
+	
+	@ManyToOne(optional = false)
+	private User user;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getGithubUrl() {
+		return githubUrl;
+	}
+	public void setGithubUrl(String githubUrl) {
+		this.githubUrl = githubUrl;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCodeReviewVideoUrl() {
+		return codeReviewVideoUrl;
+	}
+	public void setCodeReviewVideoUrl(String codeReviewVideoUrl) {
+		this.codeReviewVideoUrl = codeReviewVideoUrl;
+	}
+	public User getAssignedTo() {
+		return user;
+	}
+	public void setAssignedTo(User user) {
+		this.user = user;
+	}
+	
+	
+	
+}
+	
---
+++ src/main/java/com/devansh/AssignmentSubmissionApp/model/User.java
@@ -0,0 +1,51 @@
+package com.devansh.AssignmentSubmissionApp.model;
+
+import java.time.LocalDate;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate cohortStartDate;
+	private String userName;
+	private String pasword;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDate getCohortStartDate() {
+		return cohortStartDate;
+	}
+	public void setCohortStartDate(LocalDate cohortStartDate) {
+		this.cohortStartDate = cohortStartDate;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPasword() {
+		return pasword;
+	}
+	public void setPasword(String pasword) {
+		this.pasword = pasword;
+	}
+	
+	
+}
